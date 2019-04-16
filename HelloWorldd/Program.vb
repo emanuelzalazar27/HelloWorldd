77f95978908ce4c866113bf094b08584439a969a
Emanuel <ema008@hotmail.com>
2019-04-15 22:09:16 -0300

    modificado el mensaje

--- HelloWorldd/Program.vb
+++ HelloWorldd/Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World2019!")
+        Console.WriteLine("Hello World2019!!")
     End Sub
 End Module
